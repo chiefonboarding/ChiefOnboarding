e10fc7dc07f2f6b34c44caabc546e2ce5913a65b
Stan T <qwertyuiopqwertyuiop82@gmail.com>
2020-11-17 14:42:39 +0100

    Revert "Fix: avoid errors on sending messages to user that do not exist"
    This reverts commit 3ab9ed096e223608bfb81fa824df3458292d0a42.

--- back/slack_bot/slack.py
+++ back/slack_bot/slack.py
@@ -97,11 +97,8 @@ class Slack:
                 channel = self.channel
         if text is not None:
             blocks = self.format_simple_text(text)
-        try:
-            return self.client.chat_postMessage(channel=channel, blocks=blocks, as_user=True,
-                                                username=self.team.bot_id)
-        except:
-            return False
+        return self.client.chat_postMessage(channel=channel, blocks=blocks, as_user=True,
+                                            username=self.team.bot_id)
 
     def update_message(self, ts, blocks=None):
         if blocks is None:
