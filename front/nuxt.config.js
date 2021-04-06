fbfd27fff79553b75652dec19a7d66ddf2f9c359
GDay <1939656+GDay@users.noreply.github.com>
2021-04-07 01:16:16 +0200

    Fix undefined title

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -5,8 +5,8 @@ export default {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
-    title: process.env.npm_package_name || '',
+    titleTemplate: 'Onboarding', 
+    title: ' Onboarding Platform', 
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
