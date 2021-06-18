d9cacb6bce8f88e0373e0ebab53184858d7f975c
GDay <1939656+GDay@users.noreply.github.com>
2021-06-18 22:38:19 +0200

    Remove dead code

--- back/users/signals.py
+++
@@ -1,9 +0,0 @@
-from users.models import User, NewHireDetails
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-
-@receiver(post_save, sender=NewHireDetails)
-def generate_unique_url(sender, instance, created, **kwargs):
-    if created:
-        instance.generate_unique_url()
