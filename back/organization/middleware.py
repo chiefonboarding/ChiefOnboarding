5d592060d5c2218c7bef2e5a202978d4c9317b8f
Stan Triepels <1939656+GDay@users.noreply.github.com>
2021-08-11 20:30:25 +0200

    v1.2.12 (#96)
    * healthcheck url + allowed hosts fix
    
    * Update docker image number
    
    * Update ALLOWED_HOSTS for Heroku

---
+++ back/organization/middleware.py
@@ -0,0 +1,12 @@
+# Credits: https://stackoverflow.com/a/64623669
+from django.http import HttpResponse
+
+
+class HealthCheckMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.path == '/health':
+            return HttpResponse('ok')
+        return self.get_response(request)
