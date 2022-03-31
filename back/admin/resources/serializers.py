e9b04177d1fbc3947793b8f55d4929366a8a4ba7
GDay <1939656+GDay@users.noreply.github.com>
2022-03-31 17:40:59 +0200

    remove old serializers

--- back/admin/resources/serializers.py
+++ back/admin/resources/serializers.py
@@ -1,14 +1,7 @@
 from rest_framework import serializers
 from rest_framework.fields import SerializerMethodField
 
-from .fields import CategoryField
-from .models import Category, Chapter, Resource
-
-
-class CategorySerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Category
-        fields = "__all__"
+from .models import Chapter
 
 
 class ChapterSerializer(serializers.HyperlinkedModelSerializer):
@@ -22,40 +15,3 @@ class ChapterSerializer(serializers.HyperlinkedModelSerializer):
         return ChapterSerializer(
             Chapter.objects.filter(parent_chapter=obj).order_by("order"), many=True
         ).data
-
-
-class ResourceSerializer(serializers.HyperlinkedModelSerializer):
-    search_type = serializers.CharField(default="resource", read_only=True)
-    id = serializers.IntegerField(required=False)
-    category = CategoryField(allow_null=True)
-
-    chapters = ChapterSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Resource
-        fields = (
-            "id",
-            "name",
-            "category",
-            "course",
-            "search_type",
-            "on_day",
-            "tags",
-            "remove_on_complete",
-            "chapters",
-        )
-
-
-class ResourceCourseSerializer(ResourceSerializer):
-    class Meta:
-        model = Resource
-        fields = (
-            "id",
-            "name",
-            "category",
-            "course",
-            "on_day",
-            "tags",
-            "remove_on_complete",
-            "chapters",
-        )
