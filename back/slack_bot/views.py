aa8b4106eeeaa115a38796ded78db5b9896de7fe
GDay <1939656+GDay@users.noreply.github.com>
2022-07-09 00:45:53 +0200

    Avoid crashes when migrating

--- back/slack_bot/views.py
+++ back/slack_bot/views.py
@@ -39,10 +39,13 @@ else:
         slack_handler = SocketModeHandler(app, settings.SLACK_APP_TOKEN)
         slack_handler.connect()
     else:
-        integration = Integration.objects.filter(integration=0).first()
-        app = SlackBoltApp(
-            token=integration.token, signing_secret=integration.signing_secret
-        )
+        try:
+            integration = Integration.objects.filter(integration=0).first()
+            app = SlackBoltApp(
+                token=integration.token, signing_secret=integration.signing_secret
+            )
+        except Exception as e:
+            logger.info("Couldn't start slack app: " + str(e))
 
 
 def exception_handler(func):
