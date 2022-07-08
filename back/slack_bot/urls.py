62a11f0b1144094ea19414fda1cd497952e30a0d
Stan Triepels <1939656+GDay@users.noreply.github.com>
2022-07-09 01:02:19 +0200

    Change Slack websocket default to False (#173)

--- back/slack_bot/urls.py
+++ back/slack_bot/urls.py
@@ -1,7 +1,11 @@
+import logging
+
 from django.conf import settings
 from django.http import HttpRequest
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
+
+logger = logging.getLogger(__name__)
 
 
 @csrf_exempt
@@ -10,19 +14,21 @@ def slack_events_handler(request: HttpRequest):
 
 
 app_name = "slack"
+urlpatterns = []
+
 if not settings.SLACK_USE_SOCKET:
     from slack_bolt.adapter.django import SlackRequestHandler
 
-    from .views import app
+    try:
+        from .views import app
 
-    handler = SlackRequestHandler(app=app)
-    urlpatterns = [
-        path("bot", slack_events_handler, name="slack_events"),
-    ]
+        handler = SlackRequestHandler(app=app)
+        urlpatterns = [
+            path("bot", slack_events_handler, name="slack_events"),
+        ]
+    except Exception as e:
+        logger.error("Couldn't start slack app: " + str(e))
+
 elif settings.SLACK_APP_TOKEN != "":
     # Start websocket app
     from .views import app
-
-    urlpatterns = []
-else:
-    urlpatterns = []
