49af47140f7039b478a83da7b515605edd7784be
GDay <1939656+GDay@users.noreply.github.com>
2022-09-05 18:04:15 +0200

    typo

--- back/admin/integrations/models.py
+++ back/admin/integrations/models.py
@@ -126,7 +126,7 @@ class Integration(models.Model):
         except:  # noqa E722
             return False, "There was an unexpected error with the request"
 
-        if data.get("allow_4xx_response_status", False):
+        if data.get("raise_for_4xx_response_code", True):
             try:
                 response.raise_for_status()
             except Exception:
