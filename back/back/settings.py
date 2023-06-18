019f03463d3c6e4be41a30271ed23a47cf846cd9
Tao Chen <tachen@phys.ethz.ch>
2023-06-18 03:12:49 +0200

    use 'env.list'

--- back/back/settings.py
+++ back/back/settings.py
@@ -39,10 +39,7 @@ else:
         env("ALLOWED_HOST", default="0.0.0.0"),
     ]
 
-if env("CSRF_TRUSTED_ORIGINS", default="") != "":
-    CSRF_TRUSTED_ORIGINS = env("CSRF_TRUSTED_ORIGINS").split(",")
-else:
-    CSRF_TRUSTED_ORIGINS = []
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=[])
 
 if DEBUG:
     ALLOWED_HOSTS = ["*"]
