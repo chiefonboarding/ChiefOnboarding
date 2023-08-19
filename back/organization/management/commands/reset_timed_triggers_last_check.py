7eb3394e1b501bb0de7e419ee2d5e3bddf59391f
Stan Triepels <1939656+GDay@users.noreply.github.com>
2023-08-20 00:11:07 +0200

    Reset timed triggers check for developing to avoid timeout of worker (#338)

---
+++ back/organization/management/commands/reset_timed_triggers_last_check.py
@@ -0,0 +1,20 @@
+from organization.models import Organization
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+
+
+class Command(BaseCommand):
+    help = (
+        "Resets the timed_triggers_last_check on the org model to avoid unnecessary "
+        "trigger runs when developing"
+    )
+
+    def handle(self, *args, **options):
+        try:
+            org = Organization.objects.get()
+        except Organization.DoesNotExist:
+            # skip if org has not been created yet
+            return
+
+        org.timed_triggers_last_check = timezone.now()
+        org.save()
