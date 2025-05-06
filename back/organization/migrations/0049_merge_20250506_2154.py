2dd67c209b39c91cd8ad55b1cfde9c4268b69ccf
info@shipdocs.app <info@shipdocs.app>
2025-05-06 21:56:03 +0200

    Add merge migration for storage settings and logo URL

---
+++ back/organization/migrations/0049_merge_20250506_2154.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('organization', '0048_add_storage_settings'),
+        ('organization', '0048_organization_logo_url'),
+    ]
+
+    operations = [
+    ]
