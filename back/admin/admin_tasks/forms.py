ebef5fb7c4f3af03e8751e39fe68c5ab76551c07
Stan <1939656+GDay@users.noreply.github.com>
2025-09-23 17:34:35 +0200

    revert admin_task refactor

--- back/admin/admin_tasks/forms.py
+++ back/admin/admin_tasks/forms.py
@@ -18,6 +18,28 @@ class AdminTaskCommentForm(forms.ModelForm):
         ]
 
 
+class AdminTaskUpdateForm(forms.ModelForm):
+    date = forms.DateField(
+        label=_("Date"),
+        required=False,
+        widget=forms.DateInput(attrs={"type": "date"}, format=("%Y-%m-%d")),
+    )
+
+    def __init__(self, user, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        instance = getattr(self, "instance", None)
+        if instance is not None and instance.completed:
+            for field in self.fields:
+                self.fields[field].widget.attrs["disabled"] = True
+        self.fields[
+            "assigned_to"
+        ].queryset = get_all_managers_and_admins_for_departments_of_user(user=user)
+
+    class Meta:
+        model = AdminTask
+        fields = ["name", "assigned_to", "date", "priority"]
+
+
 class AdminTaskCreateForm(forms.ModelForm):
     comment = forms.CharField(label=_("Comment"), max_length=12500)
     date = forms.DateField(
@@ -29,21 +51,18 @@ class AdminTaskCreateForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         user = kwargs.pop("user")
         super().__init__(*args, **kwargs)
-        if "option" in self.fields:
-            self.fields["option"].initial = 0
-        if "new_hire" in self.fields:
-            self.fields[
-                "new_hire"
-            ].queryset = get_all_new_hires_for_departments_of_user(user=user)
+        self.fields["option"].initial = 0
+        self.fields[
+            "new_hire"
+        ].queryset = get_all_new_hires_for_departments_of_user(user=user)
         self.fields[
             "assigned_to"
         ].queryset = get_all_managers_and_admins_for_departments_of_user(user=user)
-        if "slack_user" in self.fields:
-            self.fields[
-                "slack_user"
-            ].queryset = get_all_managers_and_admins_for_departments_of_user_with_slack(
-                user=user
-            )
+        self.fields[
+            "slack_user"
+        ].queryset = get_all_managers_and_admins_for_departments_of_user_with_slack(
+            user=user
+        )
 
     class Meta:
         model = AdminTask
@@ -60,14 +79,3 @@ class AdminTaskCreateForm(forms.ModelForm):
         ]
 
 
-class AdminTaskUpdateForm(AdminTaskCreateForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        instance = getattr(self, "instance", None)
-        if instance is not None and instance.completed:
-            for field in self.fields:
-                self.fields[field].widget.attrs["disabled"] = True
-
-    class Meta:
-        model = AdminTask
-        fields = ["name", "assigned_to", "date", "priority"]
