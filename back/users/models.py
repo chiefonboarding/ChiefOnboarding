7458468f6b1947757da22d0ad9650418d5eba0ad
Stan <1939656+GDay@users.noreply.github.com>
2025-09-26 00:15:09 +0200

    add ordering on department model

--- back/users/models.py
+++ back/users/models.py
@@ -37,6 +37,9 @@ class Department(models.Model):
 
     name = models.CharField(max_length=255)
 
+    class Meta:
+        ordering = ("name",)
+
     def __str__(self):
         return "%s" % self.name
 
