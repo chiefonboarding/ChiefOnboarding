e3eee87103d99170fef795520f55b84dc6f76217
Stan <1939656+GDay@users.noreply.github.com>
2025-09-26 01:07:36 +0200

    explicitly set elif manager

--- back/admin/people/selectors.py
+++ back/admin/people/selectors.py
@@ -6,25 +6,28 @@ from users.models import User
 def get_new_hires_for_user(*, user: User):
     if user.is_admin:
         return User.new_hires.all()
-    else:
+    elif user.is_manager:
         return User.new_hires.filter(
             Q(departments__isnull=True) | Q(departments__in=user.departments.all())
         ).distinct()
+    return user.objects.none()
 
 
 def get_colleagues_for_user(*, user: User):
     if user.is_admin:
         return User.objects.all()
-    else:
+    elif user.is_manager:
         return User.objects.filter(
             Q(departments__isnull=True) | Q(departments__in=user.departments.all())
         ).distinct()
+    return User.objects.none()
 
 
 def get_offboarding_colleagues_for_user(*, user: User):
     if user.is_admin:
         return User.offboarding.all()
-    else:
+    elif user.is_manager:
         return User.offboarding.filter(
             Q(departments__isnull=True) | Q(departments__in=user.departments.all())
         ).distinct()
+    return User.objects.none()
